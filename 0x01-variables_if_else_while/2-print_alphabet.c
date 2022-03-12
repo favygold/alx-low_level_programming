499018f5c23a8c85b457e92b3cde6447543259e0
Favygold <simepiriyefavour@gmail.com>
2022-03-12 02:12:30 -0800

    code

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - entry point, print the alphabets
+*with only two putchar()s
+*:
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-        char letter; /*Declaring statement*/
+	int i = 97;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                putchar(letter);
-        }
-
-        putchar('\n');
-
-        return (0)
-
+	while (i < 123)
+	{
+		printf("%c", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
