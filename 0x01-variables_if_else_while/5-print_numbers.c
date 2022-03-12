d89c4cdef3330b3e71708fea4fcc3ba40609d73b
Favygold <simepiriyefavour@gmail.com>
2022-03-12 02:19:14 -0800

    code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - entry point, print the alphabets
+*with only two putchar()s
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
