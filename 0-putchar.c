e3206db331d82e763b9d7e2e795562b178a6d12d
Favygold <simepiriyefavour@gmail.com>
2022-03-15 01:27:10 -0700

    book

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+int main(void)
+
+{
+int i; /*Declaring statements*/
+char string[9] = "Putchar"; /*Arrays*/
+
+for (i = 0 ; i < 9 ; i++) /*Start for*/
+{
+_putchar(string[i]);/*display word*/
+} /*End for*/
+
+_putchar('\n');
+
+return (0);
+
+}
