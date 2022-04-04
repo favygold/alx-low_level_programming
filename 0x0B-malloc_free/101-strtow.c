851ee600ee675e67baf6dd963391c50855ac68d2
Favygold <simepiriyefavour@gmail.com>
2022-04-04 01:46:37 -0700

    coding is fun, malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
  * strtow - splits a string into words
@@ -11,33 +12,23 @@ char **strtow(char *str)
 {
 	char **ptr;
 	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
 
-	if (!str || !countWords(str))
+	if (!str || !countWords(str) || !*str)
 		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
+	ptr = malloc(sizeof(char *) * (countWords(str) + 1));
 	if (!ptr)
 		return (NULL);
-	for (i = 0; i < words; i++)
+	for (i = 0; i < countWords(str); i++)
 	{
 		start = startIndex(str, j);
 		end = endIndex(str, start);
 		len = end - start;
 		ptr[i] = malloc(sizeof(char) * (len + 1));
 		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-					i--;
-			}
-			free(ptr);
 			return (NULL);
-		}
 		for (k = 0; k < len; k++)
 			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
+		ptr[i][k++] = 0;
 		j = end + 1;
 	}
 	ptr[i] = NULL;
