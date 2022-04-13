67fb5553c7588a16b7e69389258583a6cd45a276
Favygold <simepiriyefavour@gmail.com>
2022-04-13 16:35:52 -0700

    coding is fun

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -10,6 +11,7 @@ void print_name(char *name, void(*f)(char *))
 {
 
 	if (name != NULL && f != NULL)
+		return;
 		f(name);
 
 }
